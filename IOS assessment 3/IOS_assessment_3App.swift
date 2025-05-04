89dc371b6db79ae6999eb5fc4b44d6369ea169f8
GCraddit <cncqw2000@gmail.com>
2025-05-04 15:59:41 +1000

    Initial Commit

---
+++ IOS assessment 3/IOS_assessment_3App.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_assessment_3App.swift
+//  IOS assessment 3
+//
+//  Created by GILES CHEN on 4/5/2025.
+//
+
+import SwiftUI
+
+@main
+struct IOS_assessment_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
